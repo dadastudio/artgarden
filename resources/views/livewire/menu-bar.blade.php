e72577773fb864839a370017d0f2efe5941df407
Michał Jarociński <farmazone@gmail.com>
2025-08-05 11:30:21 +0200

    s

--- resources/views/livewire/menu-bar.blade.php
+++ resources/views/livewire/menu-bar.blade.php
@@ -6,20 +6,20 @@ new class extends Component {
     //
 }; ?>
 
-<nav class="h-30 lg:h-42 relative z-10 flex items-center gap-x-6 border-b border-gray-200 max-sm:grid max-sm:grid-cols-2 sm:justify-between sm:px-8">
+<nav class="xl:h-42 relative z-10 flex h-16 items-center gap-x-6 border-b border-gray-200 max-lg:grid max-lg:grid-cols-2 md:h-28 md:justify-between md:px-8">
 
 	<img alt="ArtGardenLogo" class="flex-initial" src="{{ asset('img/logo.svg') }}">
 
-	<div class="flex h-full flex-row items-center text-sm uppercase tracking-wider text-gray-800 max-sm:hidden lg:text-base">
+	<div class="flex h-full flex-row items-center uppercase tracking-wider text-gray-800 max-lg:hidden md:text-sm xl:text-base">
 
-		<div class="flex h-full items-center border-l border-gray-200 px-5 lg:px-10"><a href="{{ route('index') }}" wire:navigate>Oferta</a></div>
-		<div class="flex h-full items-center border-l border-gray-200 px-5 lg:px-10"><a href="{{ route('index') }}" wire:navigate>Realizacje</a></div>
-		<div class="flex h-full items-center border-l border-gray-200 px-5 lg:px-10"><a href="{{ route('index') }}" wire:navigate>Blog</a></div>
-		<div class="flex h-full items-center border-l border-gray-200 px-5 lg:px-10"><a href="{{ route('index') }}" wire:navigate>Kontakt</a></div>
-		<div class="flex h-full items-center border-l border-gray-200 px-5 lg:px-10"><a href="{{ route('index') }}" wire:navigate>PL / EN</a></div>
+		<div class="flex h-full items-center border-l border-gray-200 lg:px-5 xl:px-10"><a href="{{ route('index') }}" wire:navigate>Oferta</a></div>
+		<div class="flex h-full items-center border-l border-gray-200 lg:px-5 xl:px-10"><a href="{{ route('index') }}" wire:navigate>Realizacje</a></div>
+		<div class="flex h-full items-center border-l border-gray-200 lg:px-5 xl:px-10"><a href="{{ route('index') }}" wire:navigate>Blog</a></div>
+		<div class="flex h-full items-center border-l border-gray-200 lg:px-5 xl:px-10"><a href="{{ route('index') }}" wire:navigate>Kontakt</a></div>
+		<div class="flex h-full items-center border-l border-gray-200 lg:px-5 xl:px-10"><a href="{{ route('index') }}" wire:navigate>PL / EN</a></div>
 	</div>
 
-	<div class="h-full text-sm uppercase tracking-wider text-gray-800 sm:hidden">
+	<div class="h-full text-sm uppercase tracking-wider text-gray-800 lg:hidden">
 
 		<div class="flex h-full place-content-center items-center border-l border-gray-200 px-5 text-center lg:px-10">
 			<a href="#">menu</a>
