44140ee60ca64d76117d26dfb96b7ef03469e312
Michał Jarociński <farmazone@gmail.com>
2025-08-15 06:25:29 +0200

    d

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'contact_success' => 'Thank you for your message! We will contact you soon.',
+    'send_error' => 'An error occurred while sending the message. Please try again later.',
+    'required_fields' => '* Required fields',
+    'contactform_success_detailed' => "Thank you for your message! Your submission has been successfully sent. We will contact you as soon as possible.",
+    'contactform_signoff' => 'Speak to you soon — ArtGarden',
+];
--- resources/views/livewire/contactform.blade.php
+++ resources/views/livewire/contactform.blade.php
@@ -1,8 +1,11 @@
 <?php
 
 use Livewire\Volt\Component;
+use Spatie\Honeypot\Http\Livewire\Concerns\UsesSpamProtection;
+use Spatie\Honeypot\Http\Livewire\Concerns\HoneypotData;
 
 new class extends Component {
+    use UsesSpamProtection;
     public $email;
     public $name;
     public $phone;
@@ -14,16 +17,31 @@ new class extends Component {
     public $survey;
     public $successMessage;
     public $errorMessage;
+    public HoneypotData $extraFields;
+
+    public function mount()
+    {
+        $this->extraFields = new HoneypotData();
+
+        $this->email = fake()->email();
+        $this->name = fake()->name();
+        $this->phone = fake()->phoneNumber();
+        $this->type = 'Wedding';
+        $this->date = fake()->date();
+        $this->location = fake()->city();
+        $this->additional_info = fake()->text();
+        $this->terms = true;
+    }
 
     public function rules()
     {
         return [
             'email' => 'required|email',
             'name' => 'required|string',
-            'phone' => 'nullable|string',
+            'phone' => 'required|string',
             'type' => 'required|string',
-            'date' => 'nullable|date',
-            'location' => 'nullable|string',
+            'date' => 'required|date',
+            'location' => 'required|string',
             'additional_info' => 'nullable|string',
             'terms' => 'accepted',
             'survey' => 'nullable|string',
@@ -32,36 +50,53 @@ new class extends Component {
 
     public function sendMail()
     {
+        $this->protectAgainstSpam();
         $validated = $this->validate();
         try {
             \Mail::raw("Imię i nazwisko: {$this->name}\n" . "Email: {$this->email}\n" . "Telefon: {$this->phone}\n" . "Typ wydarzenia: {$this->type}\n" . "Data: {$this->date}\n" . "Lokalizacja: {$this->location}\n" . "Dodatkowe informacje: {$this->additional_info}\n" . "Jak się dowiedział: {$this->survey}\n", function ($message) {
                 $message->to(config('mail.from.address'))->subject('Nowa wiadomość z formularza kontaktowego');
             });
             $this->reset(['email', 'name', 'phone', 'type', 'date', 'location', 'additional_info', 'terms', 'survey']);
-            $this->successMessage = __('Dziękujemy za wysłanie wiadomości! Skontaktujemy się z Tobą wkrótce.');
+            $this->successMessage = __('messages.contact_success');
             $this->errorMessage = null;
         } catch (\Exception $e) {
-            $this->errorMessage = __('Wystąpił błąd podczas wysyłania wiadomości. Spróbuj ponownie później.');
+            $this->errorMessage = __('messages.send_error');
             $this->successMessage = null;
         }
+        Flux::modal('messageModal')->show();
     }
 }; ?>
 
 <x-ui.spacer class="md:border-l md:pl-8">
 
-	@if ($successMessage)
-		<div class="mb-4 rounded bg-green-100 p-4 text-green-800">{{ $successMessage }}</div>
-	@endif
-	@if ($errorMessage)
-		<div class="mb-4 rounded bg-red-100 p-4 text-red-800">{{ $errorMessage }}</div>
-	@endif
+	<flux:modal name="messageModal">
+		<x-ui.spacer class="border border-green-400 px-8 text-center font-semibold" py type="xs">
+
+			<p class="text-[19px]">{!! __('messages.contactform_success_detailed') !!}</p>
+			<p class="text-[19px]">{!! __('messages.contactform_signoff') !!}</p>
+			<p>&nbsp;</p>
+			<div class="flex justify-center"><img alt="ArtGardenLogo" class="max-w-36" src="{{ asset('img/logo.svg') }}"></div>
+
+		</x-ui.spacer>
+		{{-- @if ($successMessage)
+			<div class="mb-4 rounded bg-green-100 p-4 text-green-800">{{ $successMessage }}</div>
+		@endif
+		@if ($errorMessage)
+			<div class="mb-4 rounded bg-red-100 p-4 text-red-800">{{ $errorMessage }}</div>
+
+			
+		@endif --}}
+
+	</flux:modal>
+
 	<x-ui.spacer type="xxs">
 
 		<h2 class="text-pretty">@lang('form.title')</h2>
-		<p class="text-xs text-gray-700">* Pola obowiązkowe</p>
+		<p class="text-xs text-gray-700">@lang('messages.required_fields')</p>
 	</x-ui.spacer>
 
 	<form wire:submit.prevent="sendMail">
+		<x-honeypot livewire-model="extraFields" />
 		<x-ui.spacer pb type="xs">
 
 			<flux:input icon="pencil" label="{{ __('form.email') }}*" size="sm" type="email" wire:model.defer="email" />
@@ -102,11 +137,11 @@ new class extends Component {
 				<h2 class="text-pretty">@lang('form.survey_title')</h2>
 
 				<flux:radio.group wire:model.defer="survey">
-					<flux:radio label="{{ __('form.survey_1') }}" value="1" />
-					<flux:radio label="{{ __('form.survey_2') }}" value="2" />
-					<flux:radio label="{{ __('form.survey_3') }}" value="3" />
-					<flux:radio label="{{ __('form.survey_4') }}" value="4" />
-					<flux:radio label="{{ __('form.survey_5') }}" value="5" />
+					<flux:radio label="{{ __('form.survey_1') }}" value="{{ __('form.survey_1') }}" />
+					<flux:radio label="{{ __('form.survey_2') }}" value="{{ __('form.survey_2') }}" />
+					<flux:radio label="{{ __('form.survey_3') }}" value="{{ __('form.survey_3') }}" />
+					<flux:radio label="{{ __('form.survey_4') }}" value="{{ __('form.survey_4') }}" />
+					<flux:radio label="{{ __('form.survey_5') }}" value="{{ __('form.survey_5') }}" />
 				</flux:radio.group>
 			</x-ui.spacer>
 
