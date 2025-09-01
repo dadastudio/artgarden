a9f2d4455399e14c0e23d1124fe0ef0719fb8469
Michał Jarociński <farmazone@gmail.com>
2025-09-01 07:11:48 +0200

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
 
 use Illuminate\Support\ServiceProvider;
 use Mcamara\LaravelLocalization\Traits\LoadsTranslatedCachedRoutes;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,16 @@ class AppServiceProvider extends ServiceProvider
 		//
 
 		RouteServiceProvider::loadCachedRoutesUsing(fn() => $this->loadCachedRoutes());
+		LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+			$switch
+				->locales(['pl', 'en'])
+
+				->labels([
+					'pl' => 'Polski',
+					'en' => 'English',
+					// Other custom labels as needed
+				]);
+		});
 
 
 	}
