edee7a4b00e2daca5b71f3db0884258e88f31195
Michał Jarociński <farmazone@gmail.com>
2025-09-16 06:30:12 +0200

    Stage all changes, commit and push

--- public/index.php
+++ public/index.php
@@ -6,15 +6,23 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+	require $maintenance;
 }
-
+$customTmp = '/home/serwer2529539/tmp';
+if (is_dir($customTmp)) {
+	putenv("TMP=$customTmp");
+	putenv("TEMP=$customTmp");
+	putenv("TMPDIR=$customTmp");
+	// UWAGA: upload_tmp_dir jest SYSTEM i nie nadpiszemy go ini_setem,
+	// ale gdy pozostaje puste, PHP używa sys_get_temp_dir(),
+	// czyli trafi do $customTmp ustawionego powyżej.
+}
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
